954f21f8a40351f6ccfbfe8f81dee835daa2bd6d
MayPyone <maypyone015@gmail.com>
2024-05-04 10:13:45 +0630

    Update autocomplete

--- src/components/search-autocomplete/index.jsx
+++ src/components/search-autocomplete/index.jsx
@@ -3,60 +3,63 @@ import Suggest from './suggest'
 import "./suggest.css"
 
 export default function SearchUser(){
-    const [userData, setUserData] = useState('');
-    const [searchParam, setSearchParam]= useState(null);
+    //https://dummyjson.com/users
+    const [userData, setUserData]= useState([]);
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     const [filterUsers, setFilterUsers] = useState([]);
+    const [searchParam, setSearchParam]= useState('');
     const [showDropdown, setShowDropdown] = useState(false);
-    async function fetchUser(){
-        setLoading(true);
+    
+    async function fetchUsers(){
+       
         try{
+            setLoading(true);
             const response = await fetch("https://dummyjson.com/users");
-            const data = await response.json();
+            const data= await response.json();
             if(data){
-            setUserData(data.users.map((item)=>item.firstName));
+                setUserData(data.users.map((item)=>(item.firstName)))
+                setLoading(false);
+                setError(null);
             }
-
-           
-
-            setLoading(false);
         }catch(error){
-          setLoading(false);
+            setError(error);
         }
-
-
     }
 
-    function handleInput(event){
-    const query = event.target.value;
+    function handleInput(event) {
+    const query =event.target.value;
+   
     setSearchParam(query);
     if(query.length>1){
-     console.log(query.length);
-     const users= userData.filter((item) => item.toLowerCase().indexOf(query.toLowerCase()) > -1)
-    console.log(users);
-    setFilterUsers(users);
+        const data= userData.filter((user)=>(user.toLowerCase().indexOf(query.toLowerCase())>-1));
+        setFilterUsers(data);
+        setShowDropdown(true);
     }
     else {
         setFilterUsers([]);
+        setShowDropdown(false)
     }
+    
+    }
+    function handleClick(event){
+        setSearchParam(event.target.innerText);
     }
 
-    function handleClick(e){
-        setSearchParam(e.target.innerText);
-        console.log(e.target.innerText);
-    }
-
-    useEffect(()=>{ fetchUser()  }
-    ,[])
-
+    useEffect(()=>{fetchUsers()},[])
+    
     if(loading){
-        return <div>Loading...</div>
+        return<div>Loading...</div>
     }
 
+    if(error){
+        return <div>{error}</div>
+    }
     return(
         <div>
-        <input value = {searchParam} type="text" placeholder="Search users..." onChange={(e)=>{handleInput(e)}} />
-        <Suggest data={filterUsers} action={handleClick}/>
+         <input type="text" onChange={handleInput} value={searchParam}
+         />
+         {showDropdown && <Suggest data={filterUsers} action={handleClick} />}
         </div>
     )
 }
